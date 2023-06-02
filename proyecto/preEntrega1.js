6137086da968656e8f63049719031ed67933c656
Juan Cruz Scadding <juancruzscadding12@gmail.com>
2023-06-02 03:53:41 -0300

    testeo para entregar

--- proyecto/preEntrega1.js
+++ proyecto/preEntrega1.js
@@ -1,29 +1,30 @@
 // Función para jugar una partida a 15 puntos
 function jugarRonda15() {
                                                     //DECLARO VARIABLES LOCALES
-  let puntajeNosotros = 0
+  let puntajeNosotros = 0 //PUNTAJES INICIALES DE CADA EQUIPO
   let puntajeEllos = 0
-  let turno = 1
+  let turno = 1 //VARIABLE PARA LUEGO ALTERNAR TURNOS
+
   alert("EMPIEZA JUEGO A 15 PUNTOS")
-                                                    //
-  while (puntajeNosotros < 15 && puntajeEllos < 15) {
-    const puntajeTurno = parseInt(prompt(`Equipo ${turno}, cuantos puntos sumaste este turno?:`))
+
+  while (puntajeNosotros < 15 && puntajeEllos < 15) { //MIENTRAS NINGUN EQUIPO SUPERE LOS 15 PUNTOS
+    const puntajeTurno = parseInt(prompt(`Equipo ${turno}, cuantos puntos sumaste este turno?:`)) //LE SOLICITO AL EQUIPO QUE INGRESE CUANTOS PUNTOS OBTUVO ESA RONDA
 
     if (isNaN(puntajeTurno)) {
-      alert("Ingresa un valor numérico válido.")
+      alert("Ingresa un valor numérico válido.") //SI NO ES UN NUMERO, VUELVE A PREGUNTAR
       continue
     }
 
     if (turno === 1) {
-      puntajeNosotros = puntajeNosotros + puntajeTurno
+      puntajeNosotros = puntajeNosotros + puntajeTurno 
+      alert(`Puntaje del Equipo 1: ${puntajeNosotros}`)
+
     } else {
-      puntajeEllos += puntajeTurno
+      puntajeEllos = puntajeEllos + puntajeTurno  
+      alert(`Puntaje del Equipo 2: ${puntajeEllos}`)
     }
 
-    alert(`Puntaje del Equipo 1: ${puntajeNosotros}`)
-    alert(`Puntaje del Equipo 2: ${puntajeEllos}`)
-
-    if (puntajeNosotros >= 15 || puntajeEllos >= 15) {
+    if (puntajeNosotros >= 15 || puntajeEllos >= 15) { //SALIR DEL BUCLE SI ALGUNO DE LOS EQUIPOS ALCANZA O SUPERA LOS 15 PUNTOS
       break
     }
                                                     //ESTE CONDICIONAL ALTERNA LOS TURNOS
@@ -36,54 +37,61 @@ function jugarRonda15() {
 
   if (puntajeNosotros >= 15) {
     alert("USTEDES GANAN (equipo 1)")
+    alert(`EL EQUIPO 2 QUEDO CON ${puntajeEllos} PUNTOS`)
   } else {
     alert("ELLOS GANAN (equipo 2)")
+    alert(`EL EQUIPO 1 QUEDO CON ${puntajeNosotros} PUNTOS`)
   }
 }
-
+                                                    //HAGO LO MISMO PERO PARA UNA PARTIDA A 30 PUNTOS
 function jugarRonda30() {
-  let puntajeNosotros = 0
-  let puntajeEllos = 0
-  let turno = 1
+                                                  //DECLARO VARIABLES LOCALES
+let puntajeNosotros = 0 //PUNTAJES INICIALES DE CADA EQUIPO
+let puntajeEllos = 0
+let turno = 1 //VARIABLE PARA LUEGO ALTERNAR TURNOS
 
-  alert("EMPIEZA JUEGO A 30 PUNTOS")
+alert("EMPIEZA JUEGO A 30 PUNTOS")
 
-  while (puntajeNosotros < 30 && puntajeEllos < 30) {
-    const puntajeTurno = parseInt(prompt(`Equipo ${turno}, ingresa los puntos que sumaron en este turno:`))
+while (puntajeNosotros < 30 && puntajeEllos < 30) {
+  const puntajeTurno = parseInt(prompt(`Equipo ${turno}, cuantos puntos sumaste este turno?:`)) 
 
-    if (isNaN(puntajeTurno)) {
-      alert('Ingresa un valor numérico válido.')
-      continue
-    }
-
-    if (turno === 1) {
-      puntajeNosotros += puntajeTurno
-    } else {
-      puntajeEllos += puntajeTurno
-    }
-
-    alert(`Puntaje del Equipo 1: ${puntajeNosotros}`)
-    alert(`Puntaje del Equipo 2: ${puntajeEllos}`)
-
-    if (puntajeNosotros >= 30 || puntajeEllos >= 30) {
-      break
-    }
-
-    if (turno === 1) {
-      turno = 2;
-    } else {
-      turno = 1;
-    }
-      }
-
-  if (puntajeNosotros >= 30 && puntajeEllos >= 30) {
-    alert('¡Empate! Ambos equipos han alcanzado los 30 puntos.')
-  } else if (puntajeNosotros >= 30) {
-    alert('¡Ustedes han ganado la ronda con 30 puntos!')
-  } else {
-    alert('¡Ellos han ganado la ronda con 30 puntos!')
+  if (isNaN(puntajeTurno)) {
+    alert("Ingresa un valor numérico válido.") 
+    continue
   }
+
+  if (turno === 1) {
+    puntajeNosotros = puntajeNosotros + puntajeTurno 
+    alert(`Puntaje del Equipo 1: ${puntajeNosotros}`)
+
+  } else {
+    puntajeEllos = puntajeEllos + puntajeTurno  
+    alert(`Puntaje del Equipo 2: ${puntajeEllos}`)
+  }
+
+  if (puntajeNosotros >= 30 || puntajeEllos >= 30) {
+    break
+  }
+                                          
+  if (turno === 1) {
+    turno = 2;
+  } else {
+    turno = 1;
+  }
+    }
+
+if (puntajeNosotros >= 30) {
+  alert("USTEDES GANAN (equipo 1)")
+  alert(`EL EQUIPO 2 QUEDO CON ${puntajeEllos} PUNTOS`)
+} else {
+  alert("ELLOS GANAN (equipo 2)")
+  alert(`EL EQUIPO 1 QUEDO CON ${puntajeNosotros} PUNTOS`)
 }
+}
+
+
+
+
 function menuPrincipal() {
   let menu = parseInt(prompt("ELIJA UN MODO DE JUEGO\n1 : PARTIDA A 15 PUNTOS\n2 : PARTIDA A 30 PUNTOS\n0 : PARA SALIR"))
   if((menu === 1) || (menu === 2)|| (menu === 0)){
@@ -93,7 +101,11 @@ function menuPrincipal() {
     menuPrincipal()
   }
 }
+
+
 const PARTIDA = menuPrincipal()
+
+                                  //LLAMO AL MENU DENTRO DE UN CONDICIONAL
 if(PARTIDA === 1){
   jugarRonda15()
 }else 
